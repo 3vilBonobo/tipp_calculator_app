18e8b0878f3074b3c703f84b3f13b4ee395125a9
3vilBonobo <iboutsika@gmail.com>
2021-07-31 15:48:50 +0300

    js variables

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 let billInput = document.querySelector('.bill');
 let buttonsInput = document.querySelectorAll('.btn');
+let customInput = document.querySelector('.custom');
 let peopleInput = document.querySelector('.people');
 let total = 0;
 let tip = 0;
 let resetBtn = document.querySelector('.reset');
+
+billInput.addEventListener('input', getBillInput);
+buttonsInput.forEach(getBtnInput);
+customInput.addEventListener('input', getCustomInput);
+peopleInput.addEventListener('input', getPeopleInput);
+resetBtn.addEventListener('click', reset);
+
+function getBillInput() {}
+
+function getBtnInput() {}
+
+function getCustomInput() {}
+
+function getPeopleInput() {}
+
+function reset() {}
+
+//Total tip = (bill * tip )/100
+
+//tipPerPerson = totalTip/people
+
+//Total/person = bill / person
